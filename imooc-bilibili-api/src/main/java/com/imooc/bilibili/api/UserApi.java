cacb1d2b9f9c934befcd713fa34d6549db81f3dc
cnjszzw <cnjszzw@qq.com>
2024-10-07 19:26:13 +0800

    获取RSA公钥接口

---
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/UserApi.java
@@ -0,0 +1,22 @@
+package com.imooc.bilibili.api;
+
+
+import com.imooc.bilibili.domain.JsonResponse;
+import com.imooc.bilibili.service.UserService;
+import com.imooc.bilibili.service.util.RSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApi {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/rsa-pks")
+    public JsonResponse<String> getRsaPublicKey() throws Exception {
+        return JsonResponse.success(RSAUtil.getPublicKeyStr());
+    }
+}
