4e68ad61d62281a5b8932594023acdeaddb52823
cnjszzw <cnjszzw@qq.com>
2024-11-02 19:55:52 +0800

    Feature:弹幕接收和推送的功能,增加注解启动定时任务和异步的方法

--- imooc-bilibili-api/src/main/java/com/imooc/ImoocBilibiliApp.java
+++ imooc-bilibili-api/src/main/java/com/imooc/ImoocBilibiliApp.java
@@ -5,8 +5,14 @@ import com.imooc.bilibili.service.websocket.WebSocketService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+@EnableScheduling
 public class ImoocBilibiliApp {
     public static void main(String[] args) {
         ApplicationContext app = SpringApplication.run(ImoocBilibiliApp.class, args);
