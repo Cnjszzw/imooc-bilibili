ba2235789a3dbbb60a4c1a8b9803f783d0282864
mbazzw <cnjszzw@qq.com>
2026-05-24 22:57:30 +0800

    feat: TagApi 新增 POST /video-tags 端点 + Seata 依赖（Seata AT 分布式事务演示）

--- imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/TagApi.java
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/TagApi.java
@@ -1,11 +1,16 @@
 package com.imooc.bilibili.api;
+import com.imooc.bilibili.dao.VideoDao;
 import com.imooc.bilibili.domain.JsonResponse;
 import com.imooc.bilibili.domain.Tag;
+import com.imooc.bilibili.domain.VideoTag;
 import com.imooc.bilibili.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 
 @RestController
@@ -14,12 +19,26 @@ public class TagApi {
     @Autowired
     private TagService tagService;
 
+    @Autowired
+    private VideoDao videoDao;
+
     @PostMapping("/tags")
     public JsonResponse<Long> addTag(@RequestBody Tag tag){
         Long tagId = tagService.addTag(tag);
         return new JsonResponse<>(tagId);
     }
 
-
+    /**
+     * 批量创建视频-标签关联（Seata 分布式事务演示）
+     * 供 content-service 通过 Feign 调用，与视频入库组成 @GlobalTransactional
+     */
+    @PostMapping("/video-tags")
+    public JsonResponse<String> batchAddVideoTags(@RequestBody List<VideoTag> tagList) {
+        for (VideoTag tag : tagList) {
+            tag.setCreateTime(new Date());
+        }
+        videoDao.batchAddVideoTags(tagList);
+        return JsonResponse.success();
+    }
 
 }
